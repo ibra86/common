6a18bff1e0cfdd32aec7aa7fdb04a4d0b4747805
Anton Ibragimov <ibrahimov.ag@gmail.com>
2019-10-07 23:25:51 +0300

    corrected version

--- testing_practice/code.py
+++ testing_practice/code.py
@@ -1,58 +1,45 @@
-from urlparse import urlparse
-import requests
-import urllib
-import os
-import sys
+def common_elements_from_two_lists(a,b):
+	return list(set(a+b))
 
-def get_url_list(host, body):
-	lines = body.split('\n')
-	ts_url_list = []
-	for line in lines:
-		if not line.startswith('#') and line != '':
-			if line.startswith('http'):
-				ts_url_list.append(line)
-			else:
-				ts_url_list.append('%s/%s' % (host, line))
-	return ts_url_list
+def coded_task_02(s):
+	return s.count('a')
 
-def get_host(url):
-	urlgroup = urlparse(url)
-	return urlgroup.scheme + '://' + urlgroup.hostname
+def coded_task_03(num):
 
-def get_m3u8_body(url):
-	print 'read m3u8 file:', url
-	session = requests.Session()
-	adapter = requests.adapters.HTTPAdapter(pool_connections=10, pool_maxsize=10, max_retries=10)
-	session.mount('http://', adapter)
-	session.mount('https://', adapter)
-	r = session.get(url, timeout=10)
-	return r.content
+	# while True:
+	# 	if num % 3 == 0:
+	# 		num //= 3
+	# 		if num == 1:
+	# 			return True
+	# 	else:
+	# 		return False
 
-def download_ts_file(ts_url_list, download_dir):
-	i = 0
-	for ts_url in reversed(ts_url_list):
-		i += 1
-		file_name = ts_url[ts_url.rfind('/'):]
-		curr_path = '%s%s' % (download_dir, file_name)
-		print '\n[process]: %s/%s' % (i, len(ts_url_list))
-		print '[download]:', ts_url
-		print '[target]:', curr_path
-		if os.path.isfile(curr_path):
-			print '[warn]: file already exist'
-			continue
-		urllib.urlretrieve(ts_url, curr_path)
+	while (num % 3 == 0):
+		num /= 3
+	return num == 1
 
-def main(url, download_dir):
-	host = get_host(url)
-	body = get_m3u8_body(url)
-	ts_url_list = get_url_list(host, body)
-	print 'total file count:', len(ts_url_list)
-	download_ts_file(ts_url_list, download_dir)
+def coded_task_04(num):	
+	digits_list = lambda num: [int(x) for x in str(num)]
+	res = sum(digits_list(num))
+	while res >= 10:
+		res = sum(digits_list(res))
+	return res
 
-if __name__ == '__main__':
-	args = sys.argv
-	if len(args) > 2:
-		main(args[1], args[2])
-	else:
-		print 'Fail, params error, try:'
-		print 'python', args[0], 'your_m3u8_url', 'your_local_dir\n'
+	# while num >= 10:
+	# 	num = sum(divmod(num,10))
+
+def coded_task_05(input_list):
+	return [x for x in input_list if x != 0] + [x for x in input_list if x == 0]
+
+
+def coded_task_06(input_list):
+
+	for i, k in enumerate(range(len(input_list)-2)):
+		d1 = input_list[i+1] - input_list[i]
+		d2 = input_list[i+2] - input_list[i+1]
+		if d1 != d2:
+			return False
+
+	return True
+		
+
