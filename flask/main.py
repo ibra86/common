dfabd37676c00e61ae4143255fe0e080dcbc7037
Anton Ibragimov <ibrahimov.ag@gmail.com>
2019-10-25 20:44:29 +0300

    add vegetables

--- flask/main.py
+++ flask/main.py
@@ -7,17 +7,17 @@ app = Flask(__name__)
 def home():
     vegetables_tmpl = 'vegetables.html'
     fruits_tmpl = 'fruits.html'
-    return render_template('home.html', vegetables_tmpl, fruits_tmpl)
+    return render_template('home.html')#, vegetables_tmpl, fruits_tmpl)
 
 
 @app.route('/vegetables')
 def vegetables(vars=['beans', 'carrot', 'beetroot', 'cucumber']):
-    return render_template('vegetables.html', vars)
+    return render_template('vegetables.html', vars=vars)
 
 
 @app.route('/fruits')
 def fruits(vars=['melon', 'apple', 'strawberry', 'grape']):
-    return render_template('fruits.html', vars)
+    return render_template('fruits.html', vars=vars)
 
 
 if __name__ == '__main__':
---
+++ tests/tests.py
@@ -0,0 +1,9 @@
+import unittest
+
+from homework import *
+
+class RectangleTestCases(unittest.TestCase):
+	pass
+
+if __name__ == "__main__":
+	unittest.main()
