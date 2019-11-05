b4fb4ed0caf2a0ec65ec2cecb640f7365b7d15bb
Anton Ibragimov <ibrahimov.ag@gmail.com>
2019-11-05 20:05:08 +0200

    add templates and references

--- flask_lesson_intro/main.py
+++ flask_lesson_intro/main.py
@@ -1,13 +1,26 @@
+import json
+
 from flask import Flask, render_template
-from utils import get_data
+
+from flask_lesson_intro.utils import get_data
 
 app = Flask(__name__)
 
+data = get_data()
+entity_list = [d.get('title') for d in data]
 
 @app.route('/')
 def get_home_page():
-    # return render_template("home.html", data=get_data())
-    return render_template("alarm-clock.html", data=get_data())
+    return render_template("home.html", entity_list=entity_list)
+
+
+@app.route('/<entity>')
+def get_entity_page(entity):
+    # return render_template("%s.html" % entity, entity_list=entity_list)
+    entity_description = [d.get('text') for d in data for k, v in d.items() if (
+            k == 'title' and v.lower().replace(' ', '-') == entity)]
+    entity_description = next(iter(entity_description), None)
+    return render_template("entity.html", entity=entity, entity_description=entity_description, entity_list=entity_list)
 
 
 if __name__ == "__main__":
