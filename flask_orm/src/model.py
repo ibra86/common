c48be1ed1d9633c4fd5c73a05d01682cadf665c1
Anton Ibragimov <ibrahimov.ag@gmail.com>
2019-11-25 20:02:53 +0200

    add relations to models

--- flask_orm/src/model.py
+++ flask_orm/src/model.py
@@ -3,11 +3,13 @@ from db import db
 
 class RoomModel(db.Model):
     __tablename__ = 'room_table'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    number = db.Column(db.Integer, unique=True, nullable=False)
+    id = db.Column(db.Integer, autoincrement=True)
+    number = db.Column(db.Integer, primary_key=True, nullable=False)
     level = db.Column(db.Integer, nullable=False)
     status = db.Column(db.String(20), default='available', nullable=False)
     price = db.Column(db.Integer, nullable=False)
+    tenants = db.relationship('TenantModel', backref='room')
+    # tenant_id = db.Column(db.Integer, db.ForeignKey('tenant_table.id'))
 
 
 class TenantModel(db.Model):
@@ -17,8 +19,15 @@ class TenantModel(db.Model):
     passport_id = db.Column(db.String(10), unique=True, nullable=False)
     age = db.Column(db.Integer, nullable=False)
     sex = db.Column(db.String(10), nullable=False)
-    address = db.Column(db.JSON(50), nullable=False)
-    room_number = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.JSON, nullable=False)
+    room_number = db.Column(db.Integer, db.ForeignKey('room_table.number'))
+    # room_number = db.relationship('RoomModel', backref='tenant')
+
+
+stuff_to_room = db.Table('stuff_to_room',
+                         db.Column('room_number', db.Integer, db.ForeignKey('room_table.number')),
+                         db.Column('stuff_id', db.Integer, db.ForeignKey('stuff_table.id'))
+                         )
 
 
 class StuffModel(db.Model):
@@ -28,3 +37,4 @@ class StuffModel(db.Model):
     passport_id = db.Column(db.String(10), unique=True, nullable=False)
     position = db.Column(db.String(50), nullable=False)
     salary = db.Column(db.Integer, nullable=False)
+    serving = db.relationship('RoomModel', secondary=stuff_to_room, backref=db.backref('rooms'))
