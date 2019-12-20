5b4b24e5513039754099a502e6f3e7829b8c30a7
Anton Ibragimov <ibrahimov.ag@gmail.com>
2019-12-20 20:53:10 +0200

    add django-docker project init

---
+++ docker_practice/src/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/vegetables')
+def vegetables():
+	var_list = ['beans', 'carrot', 'beetroot', 'cucumber']
+	return render_template('vegetables.html', var_list=var_list)
+
+
+
+@app.route('/fruits')
+def fruits():
+	var_list=['melon', 'apple', 'strawberry', 'grape']
+	return render_template('fruits.html', var_list=var_list)
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0',debug=True)
